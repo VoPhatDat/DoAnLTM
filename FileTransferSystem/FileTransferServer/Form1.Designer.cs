c9e9860baf4f284ac362226ee13bfff275a0a680
VoPhatDat <vophatdat15032005@gmail.com>
2025-12-07 08:38:03 +0700

    Cập nhật tùy chỉnh port, giao diện cho Client và Server

--- FileTransferSystem/FileTransferServer/Form1.Designer.cs
+++ FileTransferSystem/FileTransferServer/Form1.Designer.cs
@@ -2,36 +2,25 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panelHeader = new System.Windows.Forms.Panel();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
             this.panelHeader.SuspendLayout();
             this.SuspendLayout();
@@ -45,18 +34,18 @@
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStart.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStart.ForeColor = System.Drawing.Color.White;
-            this.btnStart.Location = new System.Drawing.Point(497, 12);
+            this.btnStart.Location = new System.Drawing.Point(549, 12);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(109, 36);
+            this.btnStart.Size = new System.Drawing.Size(95, 36);
             this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "START SERVER";
+            this.btnStart.Text = "START";
             this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
@@ -65,9 +54,9 @@
             this.richTextBox1.Location = new System.Drawing.Point(12, 65);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(594, 324);
+            this.richTextBox1.Size = new System.Drawing.Size(643, 324);
             this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "Waiting for command...\n> System ready.";
+            this.richTextBox1.Text = "Waiting for configuration...\n> Set Port and click START.";
             // 
             // label1
             // 
@@ -76,21 +65,68 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 21);
+            this.label1.Size = new System.Drawing.Size(185, 21);
             this.label1.TabIndex = 2;
             this.label1.Text = "FILE TRANSFER SERVER";
             // 
             // panelHeader
             // 
             this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.panelHeader.Controls.Add(this.txtPort);
+            this.panelHeader.Controls.Add(this.label2);
+            this.panelHeader.Controls.Add(this.btnOpenFolder);
             this.panelHeader.Controls.Add(this.lblStatus);
             this.panelHeader.Controls.Add(this.label1);
             this.panelHeader.Controls.Add(this.btnStart);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(618, 59);
+            this.panelHeader.Size = new System.Drawing.Size(667, 59);
             this.panelHeader.TabIndex = 3;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPort.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold);
+            this.txtPort.ForeColor = System.Drawing.Color.Yellow;
+            this.txtPort.Location = new System.Drawing.Point(345, 19);
+            this.txtPort.MaxLength = 5;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(64, 25);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "8080";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(301, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnOpenFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpenFolder.FlatAppearance.BorderSize = 0;
+            this.btnOpenFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFolder.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFolder.ForeColor = System.Drawing.Color.White;
+            this.btnOpenFolder.Location = new System.Drawing.Point(425, 13);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(109, 36);
+            this.btnOpenFolder.TabIndex = 4;
+            this.btnOpenFolder.Text = "OPEN FOLDER";
+            this.btnOpenFolder.UseVisualStyleBackColor = false;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
             // 
             // lblStatus
             // 
@@ -99,16 +135,16 @@
             this.lblStatus.ForeColor = System.Drawing.Color.Gray;
             this.lblStatus.Location = new System.Drawing.Point(13, 33);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(105, 14);
+            this.lblStatus.Size = new System.Drawing.Size(126, 14);
             this.lblStatus.TabIndex = 3;
-            this.lblStatus.Text = "Port: 8080 | TCP";
+            this.lblStatus.Text = "Status: Listening";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.ClientSize = new System.Drawing.Size(618, 401);
+            this.ClientSize = new System.Drawing.Size(667, 401);
             this.Controls.Add(this.panelHeader);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
@@ -127,5 +163,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panelHeader;
         private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox txtPort; // Thêm biến này
+        private System.Windows.Forms.Label label2;    // Thêm biến này
     }
 }
